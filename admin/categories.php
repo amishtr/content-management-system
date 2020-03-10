140affc659faf8130993ba0ba989eefe84ee532d
gbtrivea <amish.trivedi@sony.com>
2020-03-10 11:08:37 +0000

    initial commit

--- admin/categories.php
+++ admin/categories.php
@@ -1,12 +1,9 @@
-<?php
-include "includes/admin_header.php"; 
-?>
+<?php include "includes/admin_header.php"; ?>
 
     <div id="wrapper">
 
         <!-- Navigation -->
         <?php include "includes/admin_navigation.php"; ?>
-
 
         <div id="page-wrapper">
 
@@ -22,42 +19,92 @@ include "includes/admin_header.php";
                         
                         <!-- Add Category Form -->
                         <div class="col-xs-6">
-                            <form action="">
+                        <?php 
+
+                        $cat_error = "";
+                        if(isset($_POST['submit'])) {
+
+                           $cat_title = $_POST['cat_title'];
+                           
+                           if($cat_title == "" || empty($cat_title)) {
+                               $cat_error =  "The field can not be empty.";
+                           } else {
+                               $query = "INSERT INTO categories (cat_title)"; 
+                               $query .= "VALUES('{$cat_title}') ";
+
+                               $insert_categories = mysqli_query($connection, $query); 
+
+                               if(!$insert_categories) {
+                                   die('QUERY FAILED!' . mysqli_error($connection));
+                               }
+                           }
+                        }
+                        ?>
+
+                            <form action="" method="POST">
                                 <div class="form-group">
                                     <label for="cat-title">Add Category</label>
                                     <input class="form-control" type="text" name= "cat_title">
                                 </div>
                                 <div class="form-group">
                                     <input class="btn btn-primary" type="submit" name= "submit" value="Add Category">
-                                </div>                                
+                                </div>    
+                                <div class="form-group">
+                                     <?php echo $cat_error; ?> 
+                                </div>                            
                             </form>
                         </div><!-- Add Category Form -->
 
-                        <div class="col-xs-6">
+                        <div class="col-xs-6">                    
                             <table class="table table-bordered table-hover">
                                 <thead>
-                                    <tr>
+                                    <tr class="active">
                                         <th>Id</th>
                                         <th>Category Title</th>
+                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>PHP Category</td>
-                                        <td>JavaScript Category</td>
-                                    </tr>
-                                </tbody>
-                            </table>                                           
-                        </div>
+                                <?php    
+                                // To select all the categories and display them
+                                $query = "SELECT * FROM categories";
+                                $select_categories = mysqli_query($connection, $query); 
 
+                                while($row = mysqli_fetch_assoc($select_categories)) {
+
+                                    $cat_id = $row['cat_id'];    
+                                    $cat_title = $row['cat_title'];    
+                                    
+                                    echo '<tr class="">';
+                                    echo "<td>{$cat_id}</td>";   
+                                    echo "<td>{$cat_title}</td>"; 
+                                    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                                    echo "</tr>";
+                                } 
+                                ?>                                   
+                                
+                                <?php
+                                   // Delete a category(s)
+                                   if(isset($_GET['delete'])) {
+
+                                        $get_cat_id = $_GET['delete'];
+                                        $get_delete_category = "";                                      
+
+                                            $query = "DELETE FROM categories WHERE cat_id = '{$get_cat_id}'";
+                                            $delete_category = mysqli_query($connection, $query);                                        
+                                            header("Location: categories.php");
+                                        
+                                   }                               
+                               ?>
+                                </tbody>
+                            </table>                                                                         
+                       </div>
 
                     </div>
                 </div>
                 <!-- /.row -->
-
             </div>
             <!-- /.container-fluid -->
-
         </div>
         <!-- /#page-wrapper -->
 
--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,4 +1,6 @@
 <?php
+
+ob_start();
 include "../includes/db.php"; 
 ?>
 
