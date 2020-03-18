6e3319c26278487ebab108b6db70350c13e21e5d
gbtrivea <amish.trivedi@sony.com>
2020-03-18 16:20:10 +0000

    initial commit

---
+++ admin/includes/view_all_posts.php
@@ -0,0 +1,47 @@
+<table class="table table-bordered table-hover">
+                        <thead>
+                            <tr class="active">
+                                <th class='text-center'>Id</th>
+                                <th class='text-center'>Author</th>
+                                <th class='text-center'>Title</th>
+                                <th class='text-center'>Category</th>
+                                <th class='text-center'>Status</th>
+                                <th class='text-center'>Image</th>
+                                <th class='text-center'>Tags</th>
+                                <th class='text-center'>Comments</th>
+                                <th class='text-center'>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <?php 
+                                $query = "SELECT * FROM posts";
+                                $select_posts = mysqli_query($connection, $query); 
+                            
+                                while($row = mysqli_fetch_assoc($select_posts)) {
+                            
+                                    $post_id = $row['post_id'];    
+                                    $post_author = $row['post_author']; 
+                                    $post_title = $row['post_title']; 
+                                    $post_category = $row['post_category_id']; 
+                                    $post_status = $row['post_status']; 
+                                    $post_image = $row['post_image']; 
+                                    $post_tags = $row['post_tags']; 
+                                    $post_comments = $row['post_comment_count']; 
+                                    $post_date = $row['post_date'];                                                      
+                       
+                                    echo "<tr>";
+                                    echo "<td class='text-center'>{$post_id}</td>";   
+                                    echo "<td class='text-center'>{$post_author}</td>"; 
+                                    echo "<td class='text-center'>{$post_title}</td>"; 
+                                    echo "<td class='text-center'>{$post_category}</td>"; 
+                                    echo "<td class='text-center'>{$post_status}</td>"; 
+                                    echo "<td class='text-center'><img width='100px' src='../images/$post_image' alt='image'></td>"; 
+                                    echo "<td class='text-center'>{$post_tags}</td>"; 
+                                    echo "<td class='text-center'>{$post_comments}</td>"; 
+                                    echo "<td class='text-center'>{$post_date}</td>"; 
+                                    echo "</tr>";
+                                }     
+                            ?>                                              
+                        </tbody>
+                    </table>
